1ddbce5dba30fb99426372cc8ebfe062c0b7cfd6
Justin V <justinvuser@gmail.com>
2018-07-16 14:42:01 -0400

    Twitter updated

--- liri.js
+++ liri.js
@@ -1,21 +1,49 @@
 var needed = require("dotenv").config();
+var request = require('request');
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 var keys = require('./keys.js');
 
-// var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+
 
 var command = process.argv[2];
 
-if (command = "my-tweets"){
- 
+function start(){
+if (command === "my-tweets"){
+var client = new Twitter(keys.twitter);
 
-client.get('statuses/user_timeline', { screen_name: '', count: 20 },function(error, tweets, response) {
+client.get('statuses/user_timeline', { screen_name: 'WWE', count: 20 },function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
-  }
+    for (var i = 0; i < tweets.length; i++) {
+
+      console.log('Tweet number: ' + ( i + 1 ) + '\n' + "Creation date & time: " +  tweets[i].created_at + '\n' + "Text: " + tweets[i].text + '\n');}
+    }
+
+    else{
+      console.log(err);
+    }
+    
+
 });}
 
-else if(command = "spotify-this-song"){
+else if (command === "spotify-this-song"){
+var spotify = new Spotify(keys.spotify);
+    // var query = process.argv[3];
+    spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        
+console.log(data);
+});}
 
+// else if(command = "movie-this"){
+
+// }
+
+// else if(command = "do-what-it-says"){
+
+// }
 }
+start();
